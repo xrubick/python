e3d8396c4377a0c870729c5c76542eab99cce730
xcf <xuchangfu@papayamobile.cn>
2021-04-13 17:57:31 +0800

    update main.py

--- alertmanager2es/main.py
+++ alertmanager2es/main.py
@@ -3,17 +3,12 @@ from datetime import date,datetime
 from fastapi import Body, FastAPI
 import requests
 import json
+import uvicorn
 
 es_host = os.getenv('ES_HOST','es-gcp.kiwisns.com')
 es_port = os.getenv('ES_PORT','9200')
 es_url = "http://" + es_host + ":" + es_port
 index = "alertmanager-email" + "-" + date.today().strftime("%Y-%m-%d")
-
-# timestamp = datetime.timestamp(datetime.now())
-# print(timestamp)
-# # time = datetime.fromtimestamp(timestamp)
-# time = datetime.isoformat(datetime.fromtimestamp(timestamp))
-# print(time)
 
 app = FastAPI()
 @app.post("/alert")
@@ -22,11 +17,13 @@ async def update_item( email: dict = Body(...)):
     timestamp = datetime.timestamp(datetime.now())
     alerts = msg["alerts"]
     for alert in alerts:
-        print(timestamp)
         alert["timestamp"] = datetime.isoformat(datetime.utcfromtimestamp(timestamp))
-        print(alert["timestamp"])
+        del alert["generatorURL"]
         r = requests.post(es_url + "/" + index + "/_doc/?pretty",data=json.dumps(alert),headers={'Content-Type': 'application/x-ndjson'})
         text = r.content
         respone = r.headers
         print(text,respone,alert)
     return text
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
